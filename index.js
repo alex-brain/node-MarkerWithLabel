ff7e628dc7071d5ba9c66012b1c183bd377353c8
alex-brain <cm-brain@yandex.ru>
2018-03-13 15:23:39 +0300

    Fix remove child error

--- index.js
+++ index.js
@@ -322,12 +322,18 @@ module.exports = function(gMapsApi) {
    */
   MarkerLabel_.prototype.onRemove = function () {
     var i;
-    this.labelDiv_.parentNode.removeChild(this.labelDiv_);
-    this.eventDiv_.parentNode.removeChild(this.eventDiv_);
+    if (this.labelDiv_.parentNode){
+      this.labelDiv_.parentNode.removeChild(this.labelDiv_);
+    }
+    if (this.eventDiv_.parentNode) {
+      this.eventDiv_.parentNode.removeChild(this.eventDiv_);
+    }
 
     // Remove event listeners:
-    for (i = 0; i < this.listeners_.length; i++) {
-      gMapsApi.event.removeListener(this.listeners_[i]);
+    if (this.listeners_ && this.listeners_.length) {
+      for (i = 0; i < this.listeners_.length; i++) {
+        gMapsApi.event.removeListener(this.listeners_[i]);
+      }
     }
   };
 
